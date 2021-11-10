473285b81db8eef8ad31140493337b6328671308
Márk Fazekas <coldrex.fazekas.mark@gmail.com>
2021-11-10 16:05:39 +0100

    Bug fix on modal project

--- js-1-feladatok-modal/main.js
+++ js-1-feladatok-modal/main.js
@@ -6,21 +6,26 @@ const modalClose = document.querySelectorAll(".btn__modalClose");
 
 modalOpen.addEventListener('click', () => {
     setTimeout(() => {
-        modalContainer.classList.add('modal__visible');
         modalContainer.classList.remove('modal__hidden');
+        // Ha egyidőben kerül le a display none és kerül rá az opacity 1, akkor eleve opacity 1-el 
+        //  renderelődik ki, ezért minimum 1 ms-ig, opacity 0-s lesz az elem.
+        setTimeout(() => modalContainer.classList.add('modal__visible'), 1);
         modalClose[0].focus();    
     }, 1)
 });
 
 modalClose.forEach(element => element.addEventListener('click', () => {
-    modalContainer.classList.remove('modal__visible');
-    modalContainer.classList.add('modal__hidden');
+    closeModal();
 }))
+
+function closeModal(){
+    modalContainer.classList.remove('modal__visible');
+    setTimeout(() => modalContainer.classList.add('modal__hidden'), 300);
+}
 
 modalContainer.addEventListener('click', e => {
     if (!document.querySelector(".modal__content").contains(e.target)) {
-        modalContainer.classList.remove('modal__visible');
-        setTimeout(() => modalContainer.classList.add('modal__hidden'), 300);
+        closeModal();
     }
 });
 
